80806695fdca547837e13a8b682e7a9947b2eb31
KolumbanA <attila99kolumban@gmail.com>
2022-03-28 18:04:23 +0300

    AddedReadCubeScene

---
+++ RubiksCube/Assets/SceneSwitcher.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void CubeScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+    public void ReadCubeScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
