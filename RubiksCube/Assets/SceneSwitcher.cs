42433db66162f33153868a48b2b4579336c80625
KolumbanA <attila99kolumban@gmail.com>
2023-05-09 23:40:22 +0300

    algorithms view toggle

--- RubiksCube/Assets/SceneSwitcher.cs
+++ RubiksCube/Assets/SceneSwitcher.cs
@@ -5,6 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class SceneSwitcher : MonoBehaviour
 {
+    public GameObject solveButton;
+    public GameObject randomizeButton;
+    public GameObject algorithmsButton;
+    public GameObject algorithmsSelectButton;
+    public GameObject algorithmsNextButton;
+    public GameObject algorithmsPreviousButton;
+
+    private bool algorithmsViewActive = false;
+
+    private void Start()
+    {
+        algorithmsViewActive = false;
+        randomizeButton.SetActive(true);
+        solveButton.SetActive(true);
+        algorithmsSelectButton.SetActive(false);
+        algorithmsNextButton.SetActive(false);
+        algorithmsPreviousButton.SetActive(false);
+    }
     public void CubeScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
@@ -12,5 +30,24 @@ public class SceneSwitcher : MonoBehaviour
     public void ReadCubeScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void SwitchToAlgorithmsView()
+    {
+        if(algorithmsViewActive) {
+            algorithmsViewActive = false;
+            randomizeButton.SetActive(true);
+            solveButton.SetActive(true);
+            algorithmsSelectButton.SetActive(false);
+            algorithmsNextButton.SetActive(false);
+            algorithmsPreviousButton.SetActive(false);
+        } else {
+            algorithmsViewActive = true;
+            randomizeButton.SetActive(false);
+            solveButton.SetActive(false);
+            algorithmsSelectButton.SetActive(true);
+            algorithmsNextButton.SetActive(true);
+            algorithmsPreviousButton.SetActive(true);
+        }
     }
 }
